ced57b8eb29f930f32ed9e8fdfa6bf03479cbc50
Gerald <53179363+Gercai@users.noreply.github.com>
2021-06-03 14:28:41 +0100

    getting started

---
+++ game/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Shoppinglist from './components/shop'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello world
+        <Shoppinglist name="dan"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
